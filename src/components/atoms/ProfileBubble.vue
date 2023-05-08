939f34b1d1af84f29eec3fa00281e647a1e76275
clementpellen <clementpellen@gmail.com>
2023-05-08 11:36:03 +0200

    gooey-effect (1)

---
+++ src/components/atoms/ProfileBubble.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="profile-bubble profile-bubble-position radius" :class="{ 'leave-island': islandClick }" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProfileBubble',
+    props: {
+        islandClick: {
+            type: Boolean,
+            default: false
+        }
+    },
+});
+</script>
+
+<style scoped lang="scss">
+.profile-bubble {
+    background-color: var(--main-dark);
+    width: var(--island-height);
+    height: var(--island-height);
+}
+
+.profile-bubble-position {
+    position: absolute;
+    top: calc(50vh - var(--island-height) / 2);
+    // left: calc(50vw + var(--island-height) / 2 + 125px);
+    left: calc(50vw + var(--island-width) / 2 - var(--island-height));
+    z-index: 1;
+}
+
+.leave-island {
+    animation: leave-island 1s forwards;
+}
+
+@keyframes leave-island {
+    0% {
+        left: calc(50vw + var(--island-width) / 2 - var(--island-height));
+    }
+
+    100% {
+        left: calc(50vw + var(--island-width) / 2 - var(--island-height) + 60px);
+    }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,31 +1,36 @@
+<!-- https://css-tricks.com/gooey-effect/ -->
+
 <template>
     <div class="home-page">
-        <DynamicIsland 
-        :impact="dynamicIslandImpact" 
-        @click="displayPortfolio = true" 
-        />
-        <PortfolioWindow 
-        v-if="displayPortfolio" 
-        @portfolio-clicked="launchDynamicIslandImpact()"
-        @close-portfolio="displayPortfolio = false" 
-        />
-        <img src="@/assets/img/apple-screen.jpg" class="background" :class="{'blur': displayPortfolio}" />
+        <!-- <DynamicIsland :impact="dynamicIslandImpact" @click="displayPortfolio = true" /> -->
+        <div class="sticky-filter" v-if="islandClick">
+            <!-- <div class="color-dodge" /> -->
+            <!-- <div class="color-burn" /> -->
+        </div>
+        <DynamicIsland :impact="dynamicIslandImpact" @click="islandClick = true" />
+        <ProfileBubble :islandClick="islandClick" />
+        <!-- <PortfolioWindow v-if="displayPortfolio" @portfolio-clicked="launchDynamicIslandImpact()"
+            @close-portfolio="displayPortfolio = false" /> -->
+        <img src="@/assets/img/apple-screen.jpg" class="background" :class="{ 'blur': displayPortfolio }" />
     </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import DynamicIsland from '@/components/molecules/DynamicIsland.vue';
-import PortfolioWindow from '@/components/organisms/PortfolioWindow.vue';
+// import PortfolioWindow from '@/components/organisms/PortfolioWindow.vue';
+import ProfileBubble from '@/components/atoms/ProfileBubble.vue';
 
 export default defineComponent({
     name: "HomePage",
     components: {
         DynamicIsland,
-        PortfolioWindow
+        // PortfolioWindow,
+        ProfileBubble,
     },
     data() {
         return {
+            islandClick: false,
             displayPortfolio: false,
             dynamicIslandImpact: false,
         }
@@ -47,7 +52,36 @@ export default defineComponent({
 .home-page {
     width: 100%;
     height: 100%;
-    
+
+    .sticky-filter {
+        position: relative;
+        top: calc(50vh - var(--island-height) / 2 - 10px);
+        left: calc(50vw - var(--island-width) / 2 - 10px);
+        width: calc(var(--island-width) + var(--island-height) + 60px);
+        height: calc(var(--island-height) + 20px);
+        backdrop-filter: blur(10px);
+        mix-blend-mode: color-dodge;
+        z-index: 3;
+
+        // .color-dodge {
+        //     position: absolute;
+        //     width: 100%;
+        //     height: 100%;
+        //     background-color: #80808000;
+        //     mix-blend-mode: color-dodge;
+        // }
+
+        // .color-burn {
+        //     position: absolute;
+        //     top: 0;
+        //     left: 0;
+        //     width: 100%;
+        //     height: 100%;
+        //     background-color: #808080;
+        //     mix-blend-mode: color-burn;
+        // }
+    }
+
     .background {
         height: 100%;
         width: 100%;
